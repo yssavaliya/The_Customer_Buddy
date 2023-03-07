5d76ba8d9b1d9cad76f1df7d58c5f611f13f36ae
yssavaliya <ysavaliya215@rku.ac.in>
2023-03-07 17:53:09 +0530

    project with home page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,203 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  @vite('resources/css/app.css')
+</head>
+<body class="bg-[url('/images/earth.jpg')] bg-cover">
+
+<div class="p-2">
+  <div class="bg-white dark:bg-slate-800 rounded-lg px-4 py-4 ring-1 ring-slate-900/5 shadow-xl">
+  
+  <h2 class="text-slate-900 dark:text-white text-base font-medium tracking-tight">introduction</h2>
+  
+</div>
+</div>
+
+<div class="p-2">
+  <div class="bg-white dark:bg-slate-800 rounded-lg px-4 py-4 ring-1 ring-slate-900/5 shadow-xl">
+  
+  <h3 class="text-slate-900 dark:text-white text-base font-medium tracking-tight">What is SQL?</h3>
+  <p class="text-slate-500 dark:text-slate-400 mt-2 text-sm">
+    SQL is Structured Query Language used to create, maintain, destroy, update, retrive data from relational database like MYSQL, ORACLE, SQL SERVER,etc.
+  </p>
+</div>
+</div>
+
+<div class="p-2">
+  <div class="bg-white dark:bg-slate-800 rounded-lg px-4 py-4 ring-1 ring-slate-900/5 shadow-xl">
+  
+  <h3 class="text-slate-900 dark:text-white text-base font-medium tracking-tight">Why we need to learn SQL?</h3>
+  <p class="text-slate-500 dark:text-slate-400 mt-2 text-sm">
+    SQL is basic and asked question in the interviews. To become Data Scienctist or Software Developer or for laravel, .NET, etc SQL is must also we will come across the sql in the projects either personal or Academic. So, to be prepare for future we need to learn SQL  
+  </p>
+</div>
+</div>
+
+<hr class="mx-4 my-4"/>
+<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 p-2">
+  
+
+  <!-- Basic Module -->
+<div class="bg-white dark:bg-slate-800 rounded-lg px-4 py-4 ring-1 ring-slate-900/5 shadow-xl">
+    <h3 class="text-slate-900 dark:text-white text-base font-medium tracking-tight">Basics</h3>
+    <p class="text-slate-500 dark:text-slate-400 mt-2 text-sm">
+    <div class="grid grid-cols-2">
+  <ul class="list-disc px-4 dark:text-slate-400">
+    <li>Structured Query Lunguage</li>
+    <li>SQL | Datatype</li>
+    <li>SQL | DDL, DML, TCL and DCL</li>
+  </ul>
+  <ul class="list-disc px-4 dark:text-slate-400">
+    <li>SQL | Comments</li>
+    <li>CTE in SQL</li>
+    <li>SQL | TRANSACTIONS and more</li>
+  </ul>
+</div>
+
+  </p>
+  </div>
+
+
+  <!-- Clauses/Operator Module -->
+  <div class="bg-white dark:bg-slate-800 rounded-lg px-4 py-4 ring-1 ring-slate-900/5 shadow-xl">
+    <h3 class="text-slate-900 dark:text-white text-base font-medium tracking-tight">Clauses / Operators</h3>
+    <p class="text-slate-500 dark:text-slate-400 mt-2 text-sm">
+    <div class="grid grid-cols-2">
+  <ul class="list-disc px-4 dark:text-slate-400">
+    <li>SQL | WITH clause</li>
+    <li>SQL | With Ties Clause</li>
+    <li>SQL | DDL, DML, DCL and TCL Commands</li>
+  </ul>
+  <ul class="list-disc px-4 dark:text-slate-400">
+    <li>SQL | Join (Cartesian Join & Self Join)</li>
+    <li>SQL | Join (Inner, Left, Right and Full Joins)</li>
+    <li>SQL | Except Clause and more</li>
+  </ul>
+</div>
+
+  </p>
+  </div>
+
+  
+  <!-- Function Module -->
+  <div class="bg-white dark:bg-slate-800 rounded-lg px-4 py-4 ring-1 ring-slate-900/5 shadow-xl">
+    <h3 class="text-slate-900 dark:text-white text-base font-medium tracking-tight">Functions</h3>
+    <p class="text-slate-500 dark:text-slate-400 mt-2 text-sm">
+    <div class="grid grid-cols-2">
+  <ul class="list-disc px-4 dark:text-slate-400">
+    <li>SQL | Mathematical functions (SQRT, PI, SQUARE, ROUND, CEILING & FLOOR)</li>
+    <li>SQL | LISTAGG</li>
+    <li>SQL | Date functions</li>
+  </ul>
+  <ul class="list-disc px-4 dark:text-slate-400">
+    <li>SQL | Numeric Functions</li>
+    <li>SQL | Advanced Functions</li>
+    <li>SQL | Numeric Functions and more</li>
+  </ul>
+</div>
+
+  </p>
+  </div>
+
+  <!-- Queries Module -->
+  <div class="bg-white dark:bg-slate-800 rounded-lg px-4 py-4 ring-1 ring-slate-900/5 shadow-xl">
+    <h3 class="text-slate-900 dark:text-white text-base font-medium tracking-tight">Queries</h3>
+    <p class="text-slate-500 dark:text-slate-400 mt-2 text-sm">
+    <div class="grid grid-cols-2">
+  <ul class="list-disc px-4 dark:text-slate-400">
+    <li>SQL | Joining three or more tables</li>
+    <li>SQL | How to print duplicate rows in a table?</li>
+    <li>SQL | How to Get the names of the table</li>
+  </ul>
+  <ul class="list-disc px-4 dark:text-slate-400">
+    <li>SQL | How to find Nth highest salary from a table</li>
+    <li>SQL | Sub queries in From Clause</li>
+    <li>DBMS | Nested Queries in SQL and more</li>
+  </ul>
+</div>
+
+  </p>
+  </div>
+
+  <!-- SQL-Injection -->
+
+  <div class="bg-white dark:bg-slate-800 rounded-lg px-4 py-4 ring-1 ring-slate-900/5 shadow-xl">
+    <h3 class="text-slate-900 dark:text-white text-base font-medium tracking-tight">SQL-Injection</h3>
+    <p class="text-slate-500 dark:text-slate-400 mt-2 text-sm">
+    <div class="grid grid-cols-2">
+  <ul class="list-disc px-4 dark:text-slate-400">
+    <li>SQL | Joining three or more tables</li>
+    <li>SQL | How to Get the names of the table</li>
+    <li>SQL | Sub queries in From Clause</li>
+  </ul>
+  <ul class="list-disc px-4 dark:text-slate-400">
+    <li>SQL | How to print duplicate rows in a table?</li>
+    <li>SQL | How to find Nth highest salary from a table</li>
+    <li>DBMS | Nested Queries in SQL and more</li>
+  </ul>
+</div>
+
+  </p>
+  </div>
+
+    <!-- SQL-Injection -->
+  <div class="bg-white dark:bg-slate-800 rounded-lg px-4 py-4 ring-1 ring-slate-900/5 shadow-xl">
+    <h3 class="text-slate-900 dark:text-white text-base font-medium tracking-tight">PL/SQL</h3>
+    <p class="text-slate-500 dark:text-slate-400 mt-2 text-sm">
+    <div class="grid grid-cols-2">
+  <ul class="list-disc px-4 dark:text-slate-400">
+    <li>PL/SQL Introduction</li>
+    <li>Cursors in PL/SQL</li>
+    <li>Sum Of Two Numbers in PL/SQL</li>
+  </ul>
+  <ul class="list-disc px-4 dark:text-slate-400">
+    <li>Sum and average of three numbers in PL/SQL</li>
+    <li>Check whether a string is palindrome or not in PL/SQL</li>
+    <li>Count odd and even digits in a number in PL/SQL</li>
+  </ul>
+</div>
+
+  </p>
+  </div>
+
+    <!-- PL/SQL -->
+  <div class="bg-white dark:bg-slate-800 rounded-lg px-4 py-4 ring-1 ring-slate-900/5 shadow-xl">
+    <h3 class="text-slate-900 dark:text-white text-base font-medium tracking-tight">SQL Server</h3>
+    <p class="text-slate-500 dark:text-slate-400 mt-2 text-sm">
+    <div class="grid grid-cols-2">
+  <ul class="list-disc px-4 dark:text-slate-400">
+    <li>SQL SERVER | Conditional Statements</li>
+    <li>SQL Server Identity</li>
+  </ul>
+  <ul class="list-disc px-4 dark:text-slate-400">
+    <li>SQL Server | STUFF() Function</li>
+  </ul>
+</div>
+  </p>
+  </div>
+
+    <!-- MYSQL -->
+    <div class="bg-white dark:bg-slate-800 rounded-lg px-4 py-4 ring-1 ring-slate-900/5 shadow-xl">
+      <h3 class="text-slate-900 dark:text-white text-base font-medium tracking-tight">MySQL</h3>
+      <p class="text-slate-500 dark:text-slate-400 mt-2 text-sm">
+      <div class="grid grid-cols-2">
+    <ul class="list-disc px-4 dark:text-slate-400">
+      <li>MySQL | Regular expressions(Regexp)</li>
+      <li>MySQL | Grant/Revoke Privileges</li>
+      <li>MySQL | DATABASE() and CURRENT_USER() Functions</li>
+    </ul>
+    <ul class="list-disc px-4 dark:text-slate-400">
+      <li>MySQL | Change User Password</li>
+      <li>PHP | MySQL WHERE Clause</li>
+      <li>PHP | MySQL ORDER BY Clause and More</li>
+    </ul>
+        </div>
+
+    </p>
+    </div>
+</div>
+
+</body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,7 +18,7 @@
       ?>
     </p>
   </div>
-  <a href="/next-page" class="absolute top-3/4 left-1/2 inline-block px-4 py-2 font-semibold text-white border-2 border-white rounded-lg hover:bg-transparent hover:border-white hover:text-white transition-all duration-300">
+  <a href="/home" class="absolute top-3/4 left-1/2 inline-block px-4 py-2 font-semibold text-white border-2 border-white rounded-lg hover:bg-transparent hover:border-white hover:text-white transition-all duration-300">
   <span class="inline-block transform rotate-90 hover:rotate-0 transition duration-300 ease-in-out text-2xl">&#8594;</span>
 </a>
 
